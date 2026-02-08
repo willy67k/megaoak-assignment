0f787592cde6980a1968da28ae6afa9221ed1095
Lilp Su <willy67k001@gmail.com>
2026-02-09 03:37:03 +0800

    fix: leaflet image import

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,23 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import L from "leaflet";
+
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 const app = createApp(App);
 
